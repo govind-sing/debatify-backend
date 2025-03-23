464cf9296bfa9d551f747153578936670d572d27
Govind Singh <govindsinghtanwar@Govinds-MacBook-Air.local>
2025-03-24 01:33:49 +0530

    Update backend with latest changes

--- server.js
+++ server.js
@@ -47,12 +47,12 @@ app.get("/", (req, res) => {
 });
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+//   });
+// }
 
 // Global Error Handler (including multer)
 app.use((err, req, res, next) => {
